8fd2a4c33c69412c850262fcd5182e9d928b941a
EojinEva <w-w-w-@naver.com>
2024-02-01 11:54:28 +0900

    grid, bar 이미지 추가

---
+++ UIPractice/UIPractice/ProfileDesign/middleView.swift
@@ -0,0 +1,20 @@
+//
+//  middleView.swift
+//  UIPractice
+//
+//  Created by t2023-m0099 on 2/1/24.
+//
+
+import UIKit
+
+class middleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
